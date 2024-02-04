f1fb7a1db1cd3897433419b2c5d27630253c66da
Ryan Yang <y.ryan.yang@gmail.com>
2024-02-03 20:58:28 -0500

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+# Will 
+# - Generate test circuits
+# - Call the circuits which learn noise
+# - Feed the information about noise and the intended circuit to the compiler
+# - Run the compiled circuits on the noisy simulator
+
